7cab39fdcafe9019affd717d9bb385cf79c4b9d3
pfcm <pfcmathews@gmail.com>
2016-07-15 15:08:22 +1200

    stuff for doing a few of them on the grid

--- grid_search.py
+++ grid_search.py
@@ -4,10 +4,12 @@ import subprocess
 import time
 import shutil
 import logging
+import sys
+import os
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler('prelims/log')
+handler = logging.FileHandler('log')
 handler.setLevel(logging.INFO)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
@@ -17,45 +19,58 @@ logger.addHandler(handler)
 
 args = [
     'python',
-    'mnist.py',
+    os.path.abspath(
+        os.path.join(
+            os.environ['HOME'],
+            'COMP489',
+            'sequential_classifiers',
+            'mnist.py')),
     '--width=100',
     '--layers=1',
-    '--num_epochs=100',
-    '--batch_size=50',
-    '--learning_rate=0.1'
+    '--num_epochs=50',
+    '--batch_size=100',
+    '--learning_rate=0.001',
+    '--permute=True',
 ]
 
 cells = [
-    #'lstm',
-    'tt-relu',
-    'vanilla',
-    'irnn',
-    'cp-relu',
-    'cp-tanh',
-    'tt-tanh'
+    'cp+-',
+    'cp+'
 ]
 
-ranks = ['10', '50', '75']
+ranks = ['1', '5', '25'] #, '50', '75', '100']
 
 twidth = shutil.get_terminal_size((80, 20)).columns
 
-for cell, rank in itertools.product(cells, ranks):
-    if cell in ['lstm', 'vanilla', 'irnn'] \
-       and rank != ranks[0]:
-        continue
+def run_subprocess(args):
+    logger.info('starting run: %s', '\n'.join(args))
+    start = time.time()
+    subprocess.run(args, check=True)
+    end = time.time()
+    logger.info('run finished in %f s', end-start)
+    
+
+if len(sys.argv) == 1:  # full sequential search
+
+    for cell, rank in itertools.product(cells, ranks):
+        if cell in ['lstm', 'vanilla', 'irnn'] \
+           and rank != ranks[0]:
+            continue
+        unique_args = [
+            '--cell='+cell,
+            '--rank='+rank,
+            '--results_dir=prelims/highlr/{}-{}'.format(cell, rank)]
+        run_subprocess(args + unique_args)
+elif len(sys.argv) == 2:
+    # then we are just doing one and the grid has told us which
+    cell, rank = list(itertools.product(cells, ranks))[int(sys.argv[1]) - 1]
     unique_args = [
         '--cell='+cell,
         '--rank='+rank,
-        '--results_dir=prelims/highlr/{}-{}'.format(cell, rank)]
-
-    print('~' * twidth)
-    print('~' * twidth)
-    print('{:~^{}}'.format('  '+cell+'  ', twidth))
-    print('{:~^{}}'.format('  {}  '.format(rank), twidth))
-    logger.info('starting run: %s-%s', cell, rank)
-    start = time.time()
-    subprocess.run(args + unique_args, check=True)
-    end = time.time()
-    logger.info('run finished in %f s', end-start)
-    print('{:~^{}}'.format('done in {}s'.format(end-start), twidth))
-    
+        '--results_dir=output']
+    with open('model_details.txt', 'w') as fp:
+        fp.write('{}, rank {}\n'.format(cell, rank))
+    print('Doing {}, rank {}'.format(cell, rank))
+    run_subprocess(args + unique_args)
+else:
+    print(sys.argv, 'No idea.')
---
+++ grid_submission.sh
@@ -0,0 +1,60 @@
+#! /bin/sh
+
+# jic
+#$ -S /bin/sh
+
+# let me know what's up
+#$ -M Paul.Mathews@ecs.vuw.ac.nz
+#$ -m be
+
+# bail if any part freaks out
+# set -e
+
+# get the environment set up
+source $HOME/.bash_profile
+
+echo //PATH//
+echo $PATH
+
+if [ -d /local/tmp/mathewpaul1/$JOB_ID.$SGE_TASK_ID ]; then
+    cd /local/tmp/mathewpaul1/$JOB_ID.$SGE_TASK_ID
+else
+    echo "nowhere to go "
+    echo "options are: "
+    ls -la /local/tmp
+    echo "or more specifically: "
+    ls -la /local/temp/mathewpaul1
+    echo "how do you expect me to work in these conditions?"
+    exit 2
+fi
+
+# recall
+# stdout goes to:
+#    scriptname.o$JOB_ID
+
+echo //UNAME//
+uname -n
+echo //id, groups//
+id
+groups
+
+echo //where am i//
+pwd
+ls
+
+echo //where is home?//
+echo $HOME
+
+echo //setting up python environment//
+$HOME/.pyenv/bin/pyenv local py3-workspace
+
+echo //running job: $SGE_TASK_ID//
+# do a thing
+python $HOME/COMP489/sequential_classifiers/grid_search.py $SGE_TASK_ID
+
+output_dir=$HOME/COMP489/sequential_classifiers/grid_runs_permute/lowlr_$SGE_TASK_ID
+
+mkdir -p $output_dir
+
+echo //done, copying output to: $output_dir
+cp -r . $output_dir
