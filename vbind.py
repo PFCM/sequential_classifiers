df7769fe7cc0f7f2a772df39d82b4b23de9acd35
Paul Mathews <pfcmathews@gmail.com>
2016-09-04 14:16:40 +1200

    synthetics

--- vbind.py
+++ vbind.py
@@ -1,4 +1,4 @@
-"""Do the copy guy, although everyone solves him nicely"""
+"""Do some things that are a bit like variable binding"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -9,7 +9,7 @@ import tensorflow as tf
 import progressbar
 
 import mrnn
-import rnndatasets.synthetic.copy as data
+import rnndatasets.synthetic.binding as data
 
 import sequential_model as sm
 
@@ -26,6 +26,11 @@ flags.DEFINE_integer('sequence_length', 200, 'how long to remember')
 flags.DEFINE_float('max_grad_norm', 10000.0, 'how much clippery')
 
 flags.DEFINE_string('results_dir', None, 'Where to put the resuts')
+
+#problem stuff
+flags.DEFINE_string('task', 'recall', 'what to do')
+flags.DEFINE_integer('num_items', 1, 'how many things to remember')
+flags.DEFINE_integer('dimensionality', 8, 'size of patterns')
 
 FLAGS = flags.FLAGS
 
@@ -48,27 +53,32 @@ def get_cell():
 def main(_):
     """do stuff"""
 
-    inputs, targets = data.get_online_sequences(
-        FLAGS.sequence_length, FLAGS.batch_size)
+    with tf.variable_scope('inputs'):
+        inputs, targets = data.get_recognition_tensors(
+            FLAGS.batch_size, FLAGS.sequence_length, FLAGS.num_items,
+            FLAGS.dimensionality, FLAGS.task)
+        inputs = tf.unpack(inputs)
 
     print('{:-^60}'.format('getting model'), end='', flush=True)
     with tf.variable_scope('model'):
-        model_in = [tf.one_hot(tf.squeeze(input_), 9)
-                    for input_ in tf.unpack(inputs)]
-
         cell = get_cell()
-
+        if FLAGS.task == 'recall':
+            num_outputs = FLAGS.dimensionality
+        elif FLAGS.task == 'order':
+            num_outputs = FLAGS.num_items + 1
         _, _, logits, _ = sm.inference(
-            model_in, 1, cell, 9, do_projection=False,
-            full_logits=True, dynamic_iterations=8192)
+            inputs, 1, cell, num_outputs, do_projection=False,
+            full_logits=False, dynamic_iterations=0)
     print('\r{:~^60}'.format('got model'))
 
     print('{:-^60}'.format('getting training ops'), end='', flush=True)
     with tf.variable_scope('training'):
-        all_logits = tf.pack(logits)
-        loss_op = tf.reduce_mean(
-            tf.nn.sparse_softmax_cross_entropy_with_logits(
-                all_logits, tf.squeeze(targets)))
+        if FLAGS.task == 'recall':
+            loss_op = tf.reduce_mean(
+                tf.nn.sigmoid_cross_entropy_with_logits(
+                    logits, targets))
+
+
         opt = tf.train.AdamOptimizer(FLAGS.learning_rate)
         train_op = opt.minimize(loss_op)
     print('\r{:~^60}'.format('got train ops'))
